a9c8d2e45ff2f2fb576dc5146ea241702878d218
rcanai <canai3jp@gmail.com>
2020-04-06 13:47:29 +0900

    コメントを付けられるように

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ const codepipeline = new AWS.CodePipeline({apiVersion: '2015-07-09'});
 const pipelineName = process.env.PIPELINE_NAME;
 
 module.exports.handler = async function (event, context, callback) {
-	console.log('--- PROCESS START ---');
+  console.log('--- PROCESS START ---');
+
+  const { comment } = event.queryStringParameters;
+
+  console.log('COMMENT: ', comment);
 
 	const res = await codepipeline.startPipelineExecution({
 		name: pipelineName
 	}).promise();
 
+  console.log(res);
 	console.log('--- PROCESS END ---');
   return {
     statusCode: 200,
